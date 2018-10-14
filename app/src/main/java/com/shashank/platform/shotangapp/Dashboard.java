eef9bfae3d075fbbfc4968b7415d9c0202040185
shashank singhal <shashank.singhal@minemarksolutions.com>
2018-10-14 16:15:16 +0530

    Initial Commit

---
+++ app/src/main/java/com/shashank/platform/shotangapp/Dashboard.java
@@ -0,0 +1,15 @@
+package com.shashank.platform.shotangapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
